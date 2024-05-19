8df908d2a904b596f71b29ebe5988dd3292f9d4c
Meni Yakove <myakove@gmail.com>
2024-05-19 14:29:58 +0300

    wait for all dependencies to be up before starting the react server

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-set -e
+set -ep
 redis-server redis.conf &
+while ! redis-cli ping; do sleep 1; done
+echo "Redis started"
+
 poetry run python api/api.py &
+while ! curl http://127.0.0.1:5000; do sleep 1; done
+echo "API started"
+
 npm run start-server
