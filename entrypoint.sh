4ca8d47bf2a6cad7a3f152cfc62b54ba7a876b04
Meni Yakove <myakove@gmail.com>
2024-05-21 00:32:51 +0300

    Fix get claims ThreadPoolExecutor logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,13 @@ echo "Redis started"
 
 pushd api/
 poetry run python manage_users.py create_db
-poetry run uwsgi --http 0.0.0.0:5000 --wsgi-file api.py --callable app --processes 4 --threads 2 &
+
+if [[ -z $DEVELOPMENT ]]; then
+	poetry run uwsgi --post-buffering --master --enable-threads --http 0.0.0.0:5000 --wsgi-file api.py --callable app --processes 4 --threads 2 &
+else
+	poetry run python api.py &
+fi
+
 while ! curl http://127.0.0.1:5000/api/healtcheck; do sleep 1; done
 echo "API started"
 popd
