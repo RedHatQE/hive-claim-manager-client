8c8557795309ecb18c9e1cc50f6681ae087de0c2
rnetser <rnetser@redhat.com>
2024-05-28 12:22:45 +0300

    Allow persistent db

--- api/config.py
+++ api/config.py
@@ -2,15 +2,16 @@ import os
 import redis
 
 
-DB_URI = os.path.join("/tmp", "db.sqlite")
-
-
 class ApplicationConfig:
     SECRET_KEY = os.environ["HIVE_CLAIM_FLASK_APP_SECRET_KEY"]
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = os.getenv("HIVE_CLAIM_MANAGER_SQLALCHEMY_ECHO", False)
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{DB_URI}"
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "HIVE_CLAIM_FLASK_APP_DB_PATH", f"sqlite:///{os.path.join('/tmp', 'db.sqlite')}"
+    )
+
+    print(f"{SQLALCHEMY_DATABASE_URI} =====================")
 
     SESSION_TYPE = "redis"
     SESSION_PERMANENT = False
